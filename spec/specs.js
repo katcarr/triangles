5360501cbce56d3e22db49d5c61f7bd1b37d158a
katcarr <kathrynelizabethcarr@gmail.com>
2015-02-13 11:59:53 -0800

    Refactor grammar in result message

--- spec/specs.js
+++ spec/specs.js
@@ -15,9 +15,4 @@ describe("triangles", function(){
     expect(triangles(2,3,12)).to.equal("not a triangle");
   });
 
-  // it("will return 'right '+ isosceles or scalene if it is a right triangle", function(){
-  //   expect(triangles(5,12,13)).to.equal("right scalene")
-  // });
-
-
 });
